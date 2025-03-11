d305044c9e2f85b0512dc161e61be8fef7a869f6
Vlad Matiushin <particular0010abyss@gmail.com>
2025-03-11 22:54:26 +0300

    Animated Reverse Polygons

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,4 +1,4 @@
-import styles from './HeroSection.module.css'
+import styles from './HeroSection.module.css';
 
 export default function HeroSection({ isDarkMode }) {
   return (
@@ -10,32 +10,64 @@ export default function HeroSection({ isDarkMode }) {
           : "linear-gradient(to right, #e2e8f0, #cbd5e1)",
       }}
     >
-      {/* Левая половина экрана */}
-      <div
-        className="absolute inset-y-0 left-0 w-full bg-gradient-to-r blur-2xl transform-gpu"
-        aria-hidden="true"
-      >
-        {/* Левый треугольник */}
-        <div
-          className={`absolute inset-0 bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-75 animate-triangle-left ${styles['animate-triangle-left']}`}
-          style={{
-            clipPath: "polygon(0% 0%, 100% 50%, 0% 100%)", // Начальная форма
-          }}
-        />
-      </div>
+      <div>
 
-      {/* Правая половина экрана */}
-      <div
-        className="absolute inset-y-0 right-0 w-full bg-gradient-to-l blur-2xl transform-gpu"
-        aria-hidden="true"
-      >
-        {/* Правый треугольник */}
+        {/* Левая половина экрана */}
         <div
-          className={`absolute inset-0 bg-gradient-to-l from-[#6ee7b7] to-[#3b82f6] opacity-75 animate-triangle-right ${styles['animate-triangle-right']}`}
-          style={{
-            clipPath: "polygon(100% 0%, 0% 50%, 100% 100%)", // Начальная форма
-          }}
-        />
+          className="absolute inset-y-0 left-0 w-full bg-gradient-to-r blur-2xl transform-gpu"
+          aria-hidden="true"
+        >
+          {/* Левый треугольник */}
+          <div
+            className={`absolute inset-0 bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-75 ${styles['animate-triangle-left']}`}
+            style={{
+              clipPath: "polygon(0% 0%, 50% 50%, 0% 100%)", // Начальная форма
+            }}
+          />
+        </div>
+
+        {/* Правая половина экрана */}
+        <div
+          className="absolute inset-y-0 right-0 w-full bg-gradient-to-l blur-2xl transform-gpu"
+          aria-hidden="true"
+        >
+          {/* Правый треугольник */}
+          <div
+            className={`absolute inset-0 bg-gradient-to-l from-[#6ee7b7] to-[#3b82f6] opacity-75 ${styles['animate-triangle-right']}`}
+            style={{
+              clipPath: "polygon(100% 0%, 50% 50%, 100% 100%)", // Начальная форма
+            }}
+          />
+        </div>
+
+        {/* Верхняя половина экрана */}
+        <div
+          className="absolute inset-x-0 top-0 h-full bg-gradient-to-b blur-2xl transform-gpu"
+          aria-hidden="true"
+        >
+          {/* Верхний треугольник */}
+          <div
+            className={`absolute inset-0 bg-gradient-to-b from-[#590226] to-[#160d94] opacity-75 ${styles['animate-triangle-top']}`}
+            style={{
+              clipPath: "polygon(0% 0%, 50% 50%, 100% 0%)", // Форма верхнего треугольника
+            }}
+          />
+        </div>
+
+        {/* Нижняя половина экрана */}
+        <div
+          className="absolute inset-x-0 bottom-0 h-full bg-gradient-to-t blur-2xl transform-gpu"
+          aria-hidden="true"
+        >
+          {/* Нижний треугольник */}
+          <div
+            className={`absolute inset-0 bg-gradient-to-t from-[#bcfde3] to-[#0b182e] opacity-75 ${styles['animate-triangle-bottom']}`}
+            style={{
+              clipPath: "polygon(0% 100%, 50% 50%, 100% 100%)", // Форма нижнего треугольника
+            }}
+          />
+        </div>
+
       </div>
 
       {/* Основной контент */}
